baee1931f67b7d7fb7a3aa7b633148f2cf3056e2
lucifersir1 <134789637+lucifersir1@users.noreply.github.com>
2024-11-17 21:59:54 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "<rds_endpoint>";
+$servername = "php-ecommerce-db.cp6kqaq84125.us-east-1.rds.amazonaws.com:3306";
 $username = "admin";
 $password = "StrongPassord!";
 $dbname = "ecommerce";
